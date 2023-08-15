932e30da0b3d4f9bd7831c3f8be73c23a4b079bc
alor <93737038+rinaalo@users.noreply.github.com>
2023-08-15 14:15:37 +0200

    - added abilities to tokenize different types

--- calculator.c
+++ calculator.c
@@ -32,6 +32,21 @@ typedef struct Lexer {
     char *current;
 } Lexer;
 
+Token lexer_number(Lexer *lexer) {
+    while (is_number(lexer->current)) lexer->current++;
+    if (*lexer->current == '.') {
+        lexer->current++;
+        while (is_number(lexer->current)) lexer->current++;
+    }
+    return lexer_make_token(lexer, TokenType_Number);
+}
+
+Token lexer_ident(Lexer *lexer) {
+    if (isalpha(lexer->current) || *lexer->current == '_') lexer->current++;
+    while (isalnum(lexer->current) || *lexer->current == '_') lexer->current++;
+    return lexer_make_token(lexer, TokenType_Ident);
+}
+
 Token lexer_make_token(Lexer *lexer, TokenType type) {
     return(Token) {
         .type = type,
@@ -47,6 +62,22 @@ Token lexer_next_token(Lexer *lexer) {
     lexer->current = lexer->start;
     if (*lexer->current == '\0')
         return lexer_make_token(lexer, TokenType_EOF);
+    lexer->current++;
+    switch(*lexer->current) {
+        case '+': lexer->current++; return lexer_make_token(lexer, TokenType_Plus);
+        case '-': lexer->current++; return lexer_make_token(lexer, TokenType_Minus);
+        case '*': lexer->current++; return lexer_make_token(lexer, TokenType_Star);
+        case '/': lexer->current++; return lexer_make_token(lexer, TokenType_Slash);
+        case '^': lexer->current++; return lexer_make_token(lexer, TokenType_Caret);
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9': return lexer_number(lexer);
+
+        default: {
+            if (is_alpha(*lexer->current)) return lexer_ident(lexer);
+            return lexer_make_token(lexer, TokenType_Error);
+        }
+    }
 }
 
 int main() {
