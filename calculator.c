7ca3befe589e010fceee6b3e61b10b912a4742c1
decimatedTomato <greentomatopower@gmail.com>
2023-08-27 18:15:02 +0200

    - Set hard-coded input string for debugging purposes

--- calculator.c
+++ calculator.c
@@ -14,10 +14,13 @@ int main() {
     
     Parser parser = { 0 };
     while (true) {
-        fgets(input, ARRAY_LEN, stdin);
+        strcpy(input, " 1+    (9)*2 *(1-2 )   ");
+        // fgets(input, ARRAY_LEN, stdin);
         printf("Inputted string \"%s\"\n", input);
         parser_set_expression(&parser, input);
-        result = evaluate(parser_parse_expression(&parser, Precedence_Min));
+        Expression_Node *expression_tree = parser_parse_expression(&parser, Precedence_Min);
+        result = evaluate(expression_tree);
+        parser_free_expression(expression_tree);
         printf("Result: %lf\n", result);
     }
     return 0;
